0224b98d19b1bdc5f182c77e376f3a8d5c2352cb
sickteenx <bestzerzr@gmail.com>
2025-03-06 16:40:34 +0700

    รอแก้

---
+++ Assets/Scrip/AirPlane.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField]  Rigidbody rb;
+    [SerializeField]  float enginePower = 20f;
+    [SerializeField]  float liftBooster = 0.5f;
+    [SerializeField]  float drag = 0.001f;
+    [SerializeField]  float angularDrag = 0.001f;
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+        
+        Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("roll") * rollPower;
+        
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
+
+    }
+}
+                  
