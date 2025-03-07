a30478b3229b3aab1b73b84f69e9fe772b1d2ff1
sickteenx <bestzerzr@gmail.com>
2025-03-07 11:52:52 +0700

    ทำยังไงก็บินไม่ได้สักที ทำไมมมม

--- Assets/Scrip/AirPlane.cs
+++ Assets/Scrip/AirPlane.cs
@@ -1,18 +1,18 @@
 using System;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Airplane : MonoBehaviour
 {
-    [SerializeField]  Rigidbody rb;
-    [SerializeField]  float enginePower = 20f;
-    [SerializeField]  float liftBooster = 0.5f;
-    [SerializeField]  float drag = 0.001f;
-    [SerializeField]  float angularDrag = 0.001f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
     [SerializeField] float yawPower = 50f;
     [SerializeField] float pitchPower = 50f;
     [SerializeField] float rollPower = 30f;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
